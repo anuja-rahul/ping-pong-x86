f6a4c73d31a28be9624568c012203261816375fc
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-01-31 22:29:27 +0530

    timer added

--- x86ASM/example.asm
+++ x86ASM/example.asm
@@ -1,5 +1,15 @@
 ORG 0x7C00
 
+PIT_COMMAND equ 0x43
+
+PIT_CHANNEL_0 equ 0x40
+
+PIT_FREQUENCY equ 1193180
+
+DESIRED_FREQ equ 60
+
+DIVISOR equ PIT_FREQUENCY / DESIRED_FREQ
+
 start:
     xor ax, ax
     mov ds, ax
@@ -10,14 +20,20 @@ start:
     mov al, 0x13 ; 320x200 256 color mode
     int 0x10
 
-    call draw_squre
-
+    mov edi, (DRAW_START + 5000)
+    call Timer_Setup
 
     jmp $
 
-draw_squre:
+
+draw_pixel:
+    mov byte [edi], COLOR_RED
+    inc edi
+    ret
+
+draw_box:
     mov edi, DRAW_START
-    mov eax , [sq_y]
+    mov eax, [sq_y]
     mov ebx, 320
     mul ebx
     add eax, edi
@@ -45,6 +61,32 @@ draw_squre:
     jl .move_down
 
 .done:
+    ret
+
+Timer_Event:
+    call draw_pixel
+    ret
+
+timer_interrupt:
+    call Timer_Event
+    mov al, 0x20
+    out 0x20, al
+    iret
+
+Timer_Setup:
+    cli
+    mov al, 00110100b ; Channel 0, lobyte/hibyte, rate generator
+    out PIT_COMMAND, al
+    ; Set divisor
+    mov ax, DIVISOR
+    out PIT_CHANNEL_0, al ; low byte
+    ; mov al, ah
+    out PIT_CHANNEL_0, al ; high byte
+    ; Set up the timer ISR
+    mov word [0x0020], timer_interrupt
+    mov word [0x0022], 0x0000 ; Enable interrupts
+
+    sti
     ret
 
 
